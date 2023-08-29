717d571b0340f4026f5e9bae8b6a8ca164716acf
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2023-08-28 21:02:32 -0700

    separate out the tests

---
+++ tests/test_degrees_to_radians.py
@@ -0,0 +1,45 @@
+import pytest
+
+try:
+    import glob
+    import importlib
+    script_path = glob.glob('./../lab1.py')[0]
+    module_path = script_path[2:-3]
+    module = importlib.import_module(module_path)
+except:
+    raise Exception(
+        'No script is available. Please follow the assignment instructions.')
+
+try:
+    degrees_to_radians = module.degrees_to_radians
+    # vowel_count = module.vowel_count
+    # triangle_hypotenuse = module.triangle_hypotenuse
+except:
+    raise Exception(
+        'Please ensure all required functions have been implemented.')
+
+
+@pytest.mark.parametrize('degree, radian', [
+    (1, 0.017),
+    (108, 1.885),
+    (0, 0),
+    (360, 6.283),
+    (180, 3.142),
+    (90, 1.571),
+    (45, 0.785),
+    (30, 0.524),
+    (60, 1.047),
+    (120, 2.094),
+    (150, 2.618),
+    (240, 4.189),
+    (270, 4.712),
+    (300, 5.236),
+    (330, 5.759),
+    (45, 0.785),
+    (135, 2.356),
+    (225, 3.927),
+    (315, 5.498),
+    (405, 7.069),
+])
+def test_degrees_to_radians(degree, radian):
+    assert pytest.approx(degrees_to_radians(degree), abs=1e-3) == radian
---
+++ tests/test_triangle_hypotenuse.py
@@ -0,0 +1,34 @@
+import pytest
+
+try:
+    import glob
+    import importlib
+    script_path = glob.glob('./../lab1.py')[0]
+    module_path = script_path[2:-3]
+    module = importlib.import_module(module_path)
+except:
+    raise Exception(
+        'No script is available. Please follow the assignment instructions.')
+
+try:
+    # degrees_to_radians = module.degrees_to_radians
+    # vowel_count = module.vowel_count
+    triangle_hypotenuse = module.triangle_hypotenuse
+except:
+    raise Exception(
+        'Please ensure all required functions have been implemented.')
+
+
+@pytest.mark.parametrize('base, height, result', [
+    (3, 4, 5),
+    (1, 1, 1.414),
+    (0, 0, 0),
+    (1, 0, 1),
+    (0, 1, 1),
+    (2, 2, 2.828),
+    (3, 3, 4.243),
+    (4, 4, 5.657),
+    (5, 5, 7.071)
+])
+def test_triangle_hypotenuse(base, height, result):
+    assert pytest.approx(triangle_hypotenuse(base, height), abs=1e-3) == result
